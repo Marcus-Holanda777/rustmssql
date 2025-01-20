ec63024b2cb4c83c48cfdf4f2bd406f7df018719
Marcus Holanda <mvsh777@hotmail.com>
2025-01-20 13:52:23 -0300

    cli integrado

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use anyhow::Ok;
+use clap::Parser;
+use tiberius::{Query, QueryStream};
 
 mod connections;
 pub use connections::*;
@@ -6,26 +8,51 @@ mod schema_file;
 pub use schema_file::*;
 use std::sync::Arc;
 
+/// Executa uma query no servidor e gera um arquivo parquet com o resultado
+#[derive(Parser)]
+struct Cli {
+    /// nome do servidor
+    #[arg(short, long)]
+    name_server: String,
+    /// query a ser executada
+    #[arg(short, long)]
+    query: String,
+    /// arquivo parquet de saída
+    #[arg(short, long, default_value = "result_query.parquet")]
+    file_parquet: String,
+    /// parametro de condicoes do programa
+    parameters: Vec<String>,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let name_server = "server";
-    let query = r#"query"#;
+    let cli: Cli = Cli::parse();
 
-    let file_parquet = "teste.parquet";
+    println!("Servidor: {}", cli.name_server);
+    println!("Query: {}", cli.query);
+    println!("Arquivo parquet: {}", cli.file_parquet);
+    println!("Condicoes: {:#?}", cli.parameters);
 
-    let schema_sql: Vec<MSchema> = shema_mssql_query(query, name_server).await?;
+    let schema_sql: Vec<MSchema> =
+        shema_mssql_query(cli.query.as_str(), cli.name_server.as_str()).await?;
     let schema = create_schema_parquet(&schema_sql);
 
-    //let mut buf = Vec::new();
-    //printer::print_schema(&mut buf, &schema);
+    let mut client = connect_server(cli.name_server.as_str()).await?;
 
-    //let string_schema = String::from_utf8(buf)?;
-    //println!("{}", string_schema);
+    let mut select: Query<'_> = Query::new(cli.query);
+    for param in cli.parameters {
+        select.bind(param);
+    }
 
-    let mut client = connect_server(name_server).await?;
-    let stream = client.query(query, &[]).await?;
+    let stream: QueryStream<'_> = select.query(&mut client).await?;
 
-    write_parquet_from_stream(stream, Arc::new(schema), &schema_sql, file_parquet).await?;
+    write_parquet_from_stream(
+        stream,
+        Arc::new(schema),
+        &schema_sql,
+        cli.file_parquet.as_str(),
+    )
+    .await?;
 
     Ok(())
 }
